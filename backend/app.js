ee033e0cd31caa03ce53e0eb32032e50c5449f90
Altaf <altafpatel8485@gmail.com>
2025-01-23 11:54:31 +0530

    setup backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require('cors');
+
 const productrouter = require("./router/ProductRoutes");
 
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(
+    cors({
+      origin: 'https://furniroaltafpatel.vercel.app', // Replace with your Vercel frontend URL
+      credentials: true, // If using cookies or other credentials
+    })
+  );
 app.use(express.json());
 app.use(bodyParser.json());
 
